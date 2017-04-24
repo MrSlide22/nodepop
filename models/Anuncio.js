42fc1e2b3dcb7b9c335d2115b2bc35e132fe8254
Pablo Aragón <pablo.aragon22@gmail.com>
2017-04-24 12:39:08 +0200

    DB script

---
+++ models/Anuncio.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// creamos un esquema
+const anuncioSchema = mongoose.Schema({
+    nombre: String,
+    venta: Boolean,
+    precio: Number,
+    foto: String,
+    tags: [String]
+});
+
+// creamos metodo estatico en el modelo
+// para recuperar lista de agentes con filtros
+anuncioSchema.statics.list = function (
+    criterios, skip, limit, select, sort,
+    callback) {
+
+    const query = Anuncio.find(criterios);
+    query.skip(skip);
+    query.limit(limit);
+    query.select(select);
+    query.sort(sort);
+    query.exec(callback);
+}
+
+// creamos el modelo de agente
+var Anuncio = mongoose.model('Anuncio', anuncioSchema);
